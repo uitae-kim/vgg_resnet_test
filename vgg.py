daae2eb36a0cd0b340f1970f946e9a296e4a6d02
DavidJ2021 <jjeong2021@chadwickschool.org>
2019-12-19 14:15:59 +0900

    Implemented VGG16 for CIFAR10

---
+++ vgg.py
@@ -0,0 +1,48 @@
+import keras
+from keras.layers import Conv2D, Flatten, Dropout, MaxPool2D, Dense
+from keras.models import Sequential
+
+class VGG16(Sequential):
+    def __init__(self):
+        super().__init__(name="VGG16")
+        self.conv1_layer()
+        self.conv2_layer()
+        self.conv3_layer()
+        self.conv4_layer()
+        self.conv5_layer()
+        self.fc_layer()
+
+    def conv1_layer(self):
+        self.add(Conv2D(64, input_shape=(32,32,3), kernel_size=(3, 3), padding='same', activation='relu'))
+        self.add(Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.add(MaxPool2D(strides=2))
+
+    def conv2_layer(self):
+        self.add(Conv2D(128, kernel_size=(3,3), padding='same', activation='relu'))
+        self.add(Conv2D(128, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.add(MaxPool2D(strides=2))
+
+    def conv3_layer(self):
+        self.add(Conv2D(256, kernel_size=(3,3), padding='same', activation='relu'))
+        self.add(Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.add(Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.add(MaxPool2D(strides=2))
+
+    def conv4_layer(self):
+        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.add(MaxPool2D(strides=2))
+
+    def conv5_layer(self):
+        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.add(MaxPool2D(strides=2))
+
+    def fc_layer(self):
+        self.add(Flatten())
+        self.add(Dropout(0.5))
+        self.add(Dense(4096))
+        self.add(Dropout(0.5))
+        self.add(Dense(10, activation='softmax'))
