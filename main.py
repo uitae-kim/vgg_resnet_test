97d9891591d1634bef13185c5cf99fc1f191a2a5
UiTae Kim <taejja09@gmail.com>
2019-12-19 14:27:02 +0900

    implemented argv use for epoch

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import keras
 from vgg import VGG16
+import sys
 
-dataset = keras.datasets.cifar10
-(x_train, y_train), (x_test, y_test) = dataset.load_data()
 
-x_train = x_train / 255.0
-x_test = x_test / 255.0
-y_train = keras.utils.to_categorical(y_train, num_classes=10)
-y_test = keras.utils.to_categorical(y_test, num_classes=10)
+def main(argv):
+    dataset = keras.datasets.cifar10
+    (x_train, y_train), (x_test, y_test) = dataset.load_data()
 
-model = VGG16()
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-print(model.summary())
-model.fit(x_train, y_train, epochs=10)
+    x_train = x_train / 255.0
+    x_test = x_test / 255.0
+    y_train = keras.utils.to_categorical(y_train, num_classes=10)
+    y_test = keras.utils.to_categorical(y_test, num_classes=10)
+
+    model = VGG16()
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    print(model.summary())
+    model.fit(x_train, y_train, epochs=argv[1])
+
+
+if __name__=="__main__":
+    main(sys.argv)
