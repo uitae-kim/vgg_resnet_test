b0dca458d8acfb5571d0bd45c96b9af0cfe6cd62
UiTae Kim <taejja09@gmail.com>
2019-12-19 15:14:33 +0900

    modified vgg model to use l2 reg & batchNorm

--- vgg.py
+++ vgg.py
@@ -1,5 +1,5 @@
 import keras
-from keras.layers import Conv2D, Flatten, Dropout, MaxPool2D, Dense
+from keras.layers import Conv2D, Flatten, BatchNormalization, Dropout, MaxPool2D, Dense
 from keras.models import Sequential
 
 class VGG16(Sequential):
@@ -13,36 +13,48 @@ class VGG16(Sequential):
         self.fc_layer()
 
     def conv1_layer(self):
-        self.add(Conv2D(64, input_shape=(32,32,3), kernel_size=(3, 3), padding='same', activation='relu'))
-        self.add(Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu'))
-        self.add(MaxPool2D(strides=2))
+        self.add(Conv2D(64, input_shape=(32,32,3), kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=keras.regularizers.l2(0.0005)))
+        self.add(BatchNormalization())
+        self.add(Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=keras.regularizers.l2(0.0005)))
+        self.add(BatchNormalization())
+        self.add(MaxPool2D(strides=(2, 2)))
 
     def conv2_layer(self):
-        self.add(Conv2D(128, kernel_size=(3,3), padding='same', activation='relu'))
-        self.add(Conv2D(128, kernel_size=(3, 3), padding='same', activation='relu'))
-        self.add(MaxPool2D(strides=2))
+        self.add(Conv2D(128, kernel_size=(3,3), padding='same', activation='relu', kernel_regularizer=keras.regularizers.l2(0.0005)))
+        self.add(BatchNormalization())
+        self.add(Conv2D(128, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=keras.regularizers.l2(0.0005)))
+        self.add(BatchNormalization())
+        self.add(MaxPool2D(strides=(2, 2)))
 
     def conv3_layer(self):
-        self.add(Conv2D(256, kernel_size=(3,3), padding='same', activation='relu'))
-        self.add(Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu'))
-        self.add(Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu'))
-        self.add(MaxPool2D(strides=2))
+        self.add(Conv2D(256, kernel_size=(3,3), padding='same', activation='relu', kernel_regularizer=keras.regularizers.l2(0.0005)))
+        self.add(BatchNormalization())
+        self.add(Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=keras.regularizers.l2(0.0005)))
+        self.add(BatchNormalization())
+        self.add(Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=keras.regularizers.l2(0.0005)))
+        self.add(BatchNormalization())
+        self.add(MaxPool2D(strides=(2, 2)))
 
     def conv4_layer(self):
-        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
-        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
-        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
-        self.add(MaxPool2D(strides=2))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=keras.regularizers.l2(0.0005)))
+        self.add(BatchNormalization())
+        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=keras.regularizers.l2(0.0005)))
+        self.add(BatchNormalization())
+        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=keras.regularizers.l2(0.0005)))
+        self.add(BatchNormalization())
+        self.add(MaxPool2D(strides=(2, 2)))
 
     def conv5_layer(self):
-        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
-        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
-        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
-        self.add(MaxPool2D(strides=2))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=keras.regularizers.l2(0.0005)))
+        self.add(BatchNormalization())
+        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=keras.regularizers.l2(0.0005)))
+        self.add(BatchNormalization())
+        self.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=keras.regularizers.l2(0.0005)))
+        self.add(BatchNormalization())
+        self.add(MaxPool2D(strides=(2, 2)))
 
     def fc_layer(self):
         self.add(Flatten())
-        self.add(Dropout(0.5))
-        self.add(Dense(4096))
-        self.add(Dropout(0.5))
+        self.add(Dense(4096, activation='relu', kernel_regularizer=keras.regularizers.l2(0.0005)))
+        self.add(Dense(4096, activation='relu', kernel_regularizer=keras.regularizers.l2(0.0005)))
         self.add(Dense(10, activation='softmax'))
