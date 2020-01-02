01cc3c6c35775d2a1b95e20dac126231b784f905
DavidJ2021 <jjeong2021@chadwickschool.org>
2020-01-02 12:29:33 +0900

    Fixed compile error (Deleted 'normalize' in dataprocessing.py)

--- dataprocessing.py
+++ dataprocessing.py
@@ -3,11 +3,8 @@ import matplotlib.pyplot as plt
 
 def preprocess():
     dataset = keras.datasets.cifar10
+    
     (x_train, y_train), (x_test, y_test) = dataset.load_data()
-
-    x_train, x_test = normalize(x_train.astype('float32'), x_test.astype('float32'))
-    y_train = keras.utils.to_categorical(y_train, num_classes=10)
-    y_test = keras.utils.to_categorical(y_test, num_classes=10)
 
     # 2 ~ 7이 동물이 들어가있음 --> 0
     for i, y in enumerate(y_train):
